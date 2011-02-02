4203c1373766e41f7e2aa921df60e23d373bc226
lucasnodine@gmail.com <lucasnodine@gmail.com@6f092bdb-1f1f-0ac0-0c6c-db56edf2ae76>
2011-02-02 23:47:05 +0000

    Finishing cleaning up of the OpenDMS.NET rename to HttpModule.

---
+++ trunk/HttpModule/ServicePointAttribute.cs
@@ -0,0 +1,132 @@
+﻿/* Copyright 2011 the OpenDMS.NET Project (http://sites.google.com/site/opendmsnet/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace HttpModule
+{
+    /// <summary>
+    /// Represents a Service Point custom attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ServicePointAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum VerbType
+        {
+            /// <summary>
+            /// HTTP HEAD
+            /// </summary>
+            HEAD = 1,
+            /// <summary>
+            /// HTTP GET
+            /// </summary>
+            GET = 2,
+            /// <summary>
+            /// HTTP PUT
+            /// </summary>
+            PUT = 4,
+            /// <summary>
+            /// HTTP POST
+            /// </summary>
+            POST = 8,
+            /// <summary>
+            /// HTTP DELETE
+            /// </summary>
+            DELETE = 16,
+            /// <summary>
+            /// All verbs
+            /// </summary>
+            ALL = HEAD | GET | PUT | POST | DELETE,
+        }
+
+        /// <summary>
+        /// Gets or sets the verb.
+        /// </summary>
+        /// <value>
+        /// The verb.
+        /// </value>
+        public VerbType Verb { get; set; }
+        /// <summary>
+        /// Gets or sets the path.
+        /// </summary>
+        /// <value>
+        /// The path.
+        /// </value>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServicePointAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        public ServicePointAttribute(string path) 
+            : this(path, VerbType.ALL)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServicePointAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        /// <param name="verb">The verb.</param>
+        public ServicePointAttribute(string path, VerbType verb)
+        {
+            Path = path;
+            Verb = verb;
+        }
+
+        /// <summary>
+        /// Gets an integer value representing a weighted score for strength of match.  The higher the score, the more characters of the
+        /// path that match.
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        /// <param name="verb">The verb.</param>
+        /// <returns>An integer value representing a weighted score for strength of match.</returns>
+        public int GetMatchRate(string path, string verb)
+        {
+            VerbType vt = StringToVerb(verb);
+
+            if ((vt & Verb) == vt)
+                return GetPathMatchRate(path);
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Parses a <see cref="VerbType"/> from a string.
+        /// </summary>
+        /// <param name="verb">The verb.</param>
+        /// <returns>The parsed <see cref="VerbType"/>.</returns>
+        private VerbType StringToVerb(string verb)
+        {
+            return (VerbType)Enum.Parse(typeof(VerbType), verb);
+        }
+
+        /// <summary>
+        /// Gets the length of this instances Path property when the argument starts with this instance's Path
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        /// <returns>Length of this instances Path property.</returns>
+        private int GetPathMatchRate(string path)
+        {
+            if (path.StartsWith(Path))
+                return Path.Length;
+
+            return -1;
+        }
+    }
+}
---
+++ trunk/HttpModule/Storage/ResultType.cs
@@ -0,0 +1,66 @@
+﻿/* Copyright 2011 the OpenDMS.NET Project (http://sites.google.com/site/opendmsnet/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace HttpModule.Storage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ResultType
+    {
+        /// <summary>
+        /// The process failed.
+        /// </summary>
+        Failure = 0,
+        /// <summary>
+        /// The process was successful.
+        /// </summary>
+        Success,
+        /// <summary>
+        /// The resource was not found.
+        /// </summary>
+        NotFound,
+        /// <summary>
+        /// An I/O error occurred while processing.
+        /// </summary>
+        IOError,
+        /// <summary>
+        /// An error occurred while serializing.
+        /// </summary>
+        SerializationError,
+        /// <summary>
+        /// An error occurred while instantiating an object.
+        /// </summary>
+        InstantiationError,
+        /// <summary>
+        /// The resource is locked.
+        /// </summary>
+        ResourceIsLocked,
+        /// <summary>
+        /// The user does not have permission to access the resource.
+        /// </summary>
+        PermissionsError,
+        /// <summary>
+        /// The length of the data does not match the length specified in the <see cref="Common.Data.MetaAsset"/>.
+        /// </summary>
+        LengthMismatch,
+        /// <summary>
+        /// The MD5 of the data does not match the MD5 in the <see cref="Common.Data.MetaAsset"/>.
+        /// </summary>
+        Md5Mismatch
+    }
+}
