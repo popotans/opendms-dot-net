9dec0357a355699ed98154d48b37e08d1f8feb0a
lucasnodine@gmail.com <lucasnodine@gmail.com@6f092bdb-1f1f-0ac0-0c6c-db56edf2ae76>
2011-09-14 04:07:54 +0000

    Begining of the HttpModule.

---
+++ trunk/OpenDMS.HttpModule/HttpModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web;
+
+namespace OpenDMS.HttpModule
+{
+    public class HttpModule : IHttpModule
+    {
+        /// <summary>
+        /// A mapping of all service points.
+        /// </summary>
+        private ServicePointMap _map;
+        /// <summary>
+        /// The class handling the HTTP interface.
+        /// </summary>
+        private ServiceHandler _handler;
+        
+        /// <summary>
+        /// Disposes of the resources (other than memory) used by the module that implements <see cref="T:System.Web.IHttpModule"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            if (_handler != null)
+                _handler.Dispose();
+        }
+
+        /// <summary>
+        /// Initializes this HttpModule
+        /// </summary>
+        /// <param name="app">The <see cref="HttpApplication"/>.</param>
+        public void Init(HttpApplication app)
+        {
+            if (_map == null)
+            {
+                _map = new ServicePointMap();
+                _map.MapServicePoints(typeof(ServiceHandler));
+            }
+
+            if (_handler == null)
+            {
+                _handler = new ServiceHandler();
+                _handler.Init();
+            }
+
+            app.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        /// <summary>
+        /// Called when a HTTP request begins.
+        /// </summary>
+        /// <param name="s">The sender (<see cref="HttpApplication"/>).</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        public void OnBeginRequest(Object s, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)s;
+            ServicePointMapElement ele = _map.GetBestMatch(app.Request.Url.PathAndQuery, app.Request.HttpMethod);
+
+            if (ele != null)
+            {
+                ele.MethodInfo.Invoke(_handler, new object[] { app });
+            }
+        }
+    }
+}
---
+++ trunk/OpenDMS.HttpModule/ServicePointAttribute.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace OpenDMS.HttpModule
+{
+    /// <summary>
+    /// Represents a Service Point custom attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ServicePointAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum VerbType
+        {
+            /// <summary>
+            /// HTTP HEAD
+            /// </summary>
+            HEAD = 1,
+            /// <summary>
+            /// HTTP GET
+            /// </summary>
+            GET = 2,
+            /// <summary>
+            /// HTTP PUT
+            /// </summary>
+            PUT = 4,
+            /// <summary>
+            /// HTTP POST
+            /// </summary>
+            POST = 8,
+            /// <summary>
+            /// HTTP DELETE
+            /// </summary>
+            DELETE = 16,
+            /// <summary>
+            /// All verbs
+            /// </summary>
+            ALL = HEAD | GET | PUT | POST | DELETE,
+        }
+
+        /// <summary>
+        /// Gets or sets the verb.
+        /// </summary>
+        /// <value>
+        /// The verb.
+        /// </value>
+        public VerbType Verb { get; set; }
+        /// <summary>
+        /// Gets or sets the path.
+        /// </summary>
+        /// <value>
+        /// The path.
+        /// </value>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServicePointAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        public ServicePointAttribute(string path)
+            : this(path, VerbType.ALL)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServicePointAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        /// <param name="verb">The verb.</param>
+        public ServicePointAttribute(string path, VerbType verb)
+        {
+            Path = path;
+            Verb = verb;
+        }
+
+        /// <summary>
+        /// Gets an integer value representing a weighted score for strength of match.  The higher the score, the more characters of the
+        /// path that match.
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        /// <param name="verb">The verb.</param>
+        /// <returns>An integer value representing a weighted score for strength of match.</returns>
+        public int GetMatchRate(string path, string verb)
+        {
+            VerbType vt = StringToVerb(verb);
+
+            if ((vt & Verb) == vt)
+                return GetPathMatchRate(path);
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Parses a <see cref="VerbType"/> from a string.
+        /// </summary>
+        /// <param name="verb">The verb.</param>
+        /// <returns>The parsed <see cref="VerbType"/>.</returns>
+        private VerbType StringToVerb(string verb)
+        {
+            return (VerbType)Enum.Parse(typeof(VerbType), verb);
+        }
+
+        /// <summary>
+        /// Gets the length of this instances Path property when the argument starts with this instance's Path
+        /// </summary>
+        /// <param name="path">The service path.</param>
+        /// <returns>Length of this instances Path property.</returns>
+        private int GetPathMatchRate(string path)
+        {
+            if (path.StartsWith(Path))
+                return Path.Length;
+
+            return -1;
+        }
+    }
+}
